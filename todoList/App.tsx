4ec956eef0881f74cf2fa33d4ca3b5f0644c47a4
Hyacinthe <hyacinthe.briand@diiage.org>
2021-04-01 12:35:56 +0200

    feat(new task): add new task section

--- todoList/App.tsx
+++ todoList/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import Task from './component/Task';
 
 export default function App() {
@@ -14,6 +14,13 @@ export default function App() {
       <Task text="Hello 😋"></Task>
       <Task text="Salut 🤠"></Task>
       </View>
+
+      <KeyboardAvoidingView>
+        <TextInput style={styles.textInput} placeholder="Add a task"/>
+      </KeyboardAvoidingView>
+      <TouchableOpacity style={styles.button}>
+        <Text>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -32,6 +39,22 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "#FFFFFF",
     marginBottom: 20
+  },
+  textInput:{
+    backgroundColor: "#FFFFFF",
+    borderRadius:60,
+    width: 256,
+    height: 45,
+    textAlign: "center",
+    marginLeft: 15
+
+  },
+  button:{
+    backgroundColor: "#FFFFFF",
+    width: 32,
+    height: 32,
+    borderRadius: 52,
+    textAlign: "center",
   }
 });
 
